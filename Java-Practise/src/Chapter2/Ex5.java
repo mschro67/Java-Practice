46122fb1412f4d0d2c5eb29a497ca13a5c606420
Maxi <maximilian.s107@evt.schule.koeln>
2025-11-15 15:37:51 +0100

    Add files via upload

---
+++ Java-Practise/src/Chapter2/Ex5.java
@@ -0,0 +1,12 @@
+//by mschro67
+
+package Chapter2;
+
+public class Ex5{
+    public static void main(String[] args){
+        boolean a=true,b=false;
+        boolean c=(a&&b||!(b));
+
+        System.out.println("a: "+a+", b: "+b+", c: "+c);
+    }
+}
